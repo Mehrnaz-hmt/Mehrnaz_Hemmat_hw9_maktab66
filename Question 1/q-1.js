0afa1a586ecfe22ad4903a616392d4ce09cbe427
Mehrnaz <mehrnazhemmat1998@gmail.com>
2022-01-03 15:01:10 +0330

    half of first question

--- Question 1/q-1.js
+++ Question 1/q-1.js
@@ -1,17 +1,19 @@
-const convertTime12to24 = (time12h) => {
-    const [time, modifier] = time12h.split(' ');
-  
-    let [hours, minutes] = time.split(':');
-  
-    if (hours === '12') {
-      hours = '00';
-    }
-  
-    if (modifier === 'PM') {
-      hours = parseInt(hours, 10) + 12;
-    }
-  
-    return `${hours}:${minutes}`;
+const formatAMPM = (date) => {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    minutes = minutes.toString().padStart(2, '0');
+    let strTime = hours + ':' + minutes + ' ' + ampm;
+    return strTime;
   }
+    let convertNewDateToString = "";
+    
+  convertNewDateToString = formatAMPM(new Date())
+  console.log(convertNewDateToString); //turn to string your pc time
+  //--------------------------------------------------------------
   
-  console.log(convertTime12to24('02:00 PM'));
+ 
+
+
